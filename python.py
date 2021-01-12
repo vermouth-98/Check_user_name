50b265e5e2a01315b4c50c54cb1a837113729723
vermouth-98 <tuankiet.181274@gmail.com>
2021-01-12 04:31:15 +0300

    Mask number for user in the list current_name

--- python.py
+++ python.py
@@ -7,3 +7,6 @@ if new_name and current_name:
         else:
             current_name.append(value_new_name.lower())
 print(current_name)
+number_current_name = list(range(len(current_name)))
+for value_flag in range(len(current_name)):
+    print(str(number_current_name[value_flag]+1) +" is "+ current_name[value_flag])
